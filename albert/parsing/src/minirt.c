bab633729036de1e4a4413b2908083c96dcae9c6
Alberto Pardo Marti <apardo-m@car15s2.42barcelona.com>
2024-07-23 11:22:28 +0200

    read a file

--- albert/parsing/src/minirt.c
+++ albert/parsing/src/minirt.c
@@ -1,15 +1,39 @@
 #include <stdio.h>
+#include <fcntl.h>
 #include "libft.h"
 #include "ft_printf.h"
+#include "get_next_line.h"
 
 #define	BAD_ARGUMENTS "BAD ARGUMETS. Use : ./minirt NAME.rt"
+#define NO_OPEN "open() failed"
+#define NO_CLOSE "\nclose() failed" 
+
+void exitifcheckfails(int val, char *msg)
+{
+	if (val == -1)
+	{
+		perror(msg);
+		exit (EXIT_FAILURE);
+	}
+}
 
 int main(int argc, char *argv[])
 {
+	int		fd;
+	char	*line;
+
 	if (argc == 2)
 	{
-		ft_printf(argv[1]);
-		ft_printf("\n");
+		fd = open(argv[1], O_RDONLY);
+		exitifcheckfails(fd, NO_OPEN);
+		line = get_next_line(fd);
+  		while (line)
+		{
+			ft_printf("%s",line);
+			free(line);
+			line = get_next_line(fd);
+		}
+		exitifcheckfails(close(fd), NO_CLOSE);
 	}
 	else
 	{
