a44d028bc877056264d8b18fa4cfa549914965a8
Alberto Pardo Marti <apardo-m@car15s2.42barcelona.com>
2024-07-25 15:26:04 +0200

    working to create list of elements 'include/minirt.h' pass norminette

--- albert/parsing/include/minirt.h
+++ albert/parsing/include/minirt.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minirt.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apardo-m <apardo-m@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/25 14:57:56 by apardo-m          #+#    #+#             */
+/*   Updated: 2024/07/25 15:20:02 by apardo-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINIRT_H
 # define MINIRT_H
 
@@ -16,6 +28,33 @@
 # define BAD_FILETYPE "Filetype is not .rt"
 # define EXT ".rt"
 
+typedef struct s_vector3
+{
+	float	x;
+	float	y;
+	float	z;
+}	t_vector3;
+
+typedef struct s_ambientlight
+{
+	float		lightradio;
+	t_vector3	f_rgb;
+}	t_ambientlight;
+
+typedef struct s_camera
+{
+	t_vector3	viewpoint;
+	t_vector3	orient;
+	float		fov;
+}	t_camera;
+
+typedef struct s_light
+{
+	t_vector3	lightpoint;
+	float		brightratio;
+	t_vector3	f_rgb;
+}	t_light;
+
 // ------  parsing -------- //
 
 char	*cleanstringspaces(char *str);
@@ -25,6 +64,6 @@ void	exitifnotvalidfiletype(char *fname, char *ext, char *msg);
 void	exitifnortfile(char *filename);
 void	freearrstr(char **arr);
 int		ft_isspace(const char c);
-void	putarraystr(char ** arrstr);
+void	putarraystr(char **arrstr);
 
 #endif
