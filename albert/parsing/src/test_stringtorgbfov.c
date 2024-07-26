fa7a8bc39eadacad8d21beda662d81fd0af110f9
Alberto Pardo Marti <apardo-m@car15s4.42barcelona.com>
2024-07-26 19:04:48 +0200

    correct to validate '0' , '0X' , '00X' where 'X' is number

--- albert/parsing/src/test_stringtorgbfov.c
+++ albert/parsing/src/test_stringtorgbfov.c
@@ -3,7 +3,6 @@
 #define CHAR_MAX_RGB "255"
 #define CHAR_MAX_FOV "180"
 #define INT_NUMDIGITS 3
-#define MIN_INT_VALUE 0
 
 static int alldigits(char *str)
 {
@@ -18,8 +17,15 @@ static int alldigits(char *str)
 }
 
 /*
+ * 'str' must be all digits
  * True if str is a integer >= 0 and <= 'maxintval'
+ * Valid values: (X in 0 to 9)
+ * - "0"
+ * - "0X"
+ * - "00X"
+ * - "XXX"
  *
+ * If 'str' contains '+' as first char and rest are digits is NOT VALID
  */
 
 int	isstringvalueinpositiverange(char *str, char *maxintval)
@@ -30,7 +36,7 @@ int	isstringvalueinpositiverange(char *str, char *maxintval)
 	len_str = ft_strlen(str);
 	len_max = ft_strlen(maxintval);
 	if (len_str <= len_max && alldigits(str)) 
-		if (len_str == len_max && ft_strncmp(str, maxintval, len_max) <= 0)
+		if (len_str < len_max || (len_str == len_max && ft_strncmp(str, maxintval, len_max) <= 0))
 			return (1);
 	return (0);
 }
@@ -48,13 +54,15 @@ int	main(void)
 {
 
 	printest("0");
+	printest("00");
+	printest("000");
 	printest("123");
 	printest("180");
 	printest("181");
 	printest("255");
 	printest("256");
 	printest("256");
-	printest("+200");
+	printest("+20");
 	printest("2.56");
 	printest("");
 	printest("a");
