d7f2e097e01476c9af6d79f33565c15f70688281
Alberto Pardo Marti <apardo-m@car15s5.42barcelona.com>
2024-07-27 15:36:17 +0200

    working str to float on range [0.0,1.0]

---
+++ albert/parsing/src/teststrtofloat01.c
@@ -0,0 +1,135 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   teststrtofloat01.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apardo-m <apardo-m@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/27 12:03:20 by apardo-m          #+#    #+#             */
+/*   Updated: 2024/07/27 15:33:49 by apardo-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minirt.h"
+
+static size_t countchar(char *str, char c)
+{
+	size_t	i;
+	char	*tmp;
+
+	tmp = str;
+	i = 0;
+	while (tmp != NULL && *tmp != '\0')
+	{
+		if (*tmp == c)
+			i++;
+		tmp++;
+	}
+	return (i);
+}
+
+/*
+ * int isstrfloat(char *str)
+ *
+ * str is valid with or whitout one point and digits
+ * No checks limits
+ *
+ */
+
+int	isstrfloat(char *str)
+{ 
+	char	*tmp;
+
+	tmp = str;
+	if (countchar(str, '.') < 2)
+	{
+		while (tmp != NULL && *tmp != '\0' && (ft_isdigit(*tmp) || *tmp == '.'))
+			tmp++;
+		if (tmp != str && *tmp == '\0')
+			return (1);
+	}
+	return (0);
+}
+
+/*
+ * int	isstrfloatcerone(char *str)
+ * 
+ * checks if str is in range [0.0,1.0]
+ * str is valid with or whitout one point and digitis
+ * Cases: ( 'X' from 1 to 9, '*' from 1 to any number of digits in range [0,9])
+ * 
+ * No limit for number of digits after point
+ * No valid start or end point: 
+ *	- '.*'
+ *	- '*.'
+ *
+ * Cases: 
+ * 	- '1.0'  	VALID
+ * 	- '1'		VALID
+ * 	- '0'		VALID
+ * 	- '0.*'		VALID
+ * 	- '1.0*'	NO VALID
+ * 	- '1.X'  	NO VALID
+ */
+
+int	isstrfloatcerone(char *str)
+{
+	size_t	len;
+
+	len = ft_strlen(str);	
+	if (isstrfloat(str) && (*str != '.' || str[len - 1] != '.'))
+	{
+/*
+		if (len > 3 && ft_strncmp(str, "1.0", 3) == 0)
+			return (0);
+		if (len == 3 && ft_strncmp(str, "1.0", 3) > 0)
+			return (0);
+*/
+		if (len == 3 && \
+				(!ft_strncmp(str, "1.0", 3) || !ft_strncmp(str, "0.0", 3)))
+			return (1);
+		if ((*str == '0' || *str == '1') && str[1] == '\0')
+			return (1);
+		if (*str == '0' && str[1] == '.' && \
+				countchar(&str[2], '0') != (len - 2))
+			return (1);
+	}
+	return (0);
+}
+
+static void	printest(char *s)
+{
+	printf("%s ? ", s); 
+	if (isstrfloatcerone(s))
+		printf("TRUE\n");
+	else
+		printf("FALSE\n");
+}
+
+int	main(void)
+{
+	printf("------ Valid float in format range [0.0, 1.0]-------\n"),
+	printest("0.0");
+	printest("0.9");
+	printest("1.0");
+	printest("0");
+	printest("1");
+	printest("0.0500000000");
+	printf("------ No Valid float in format range [0.0, 1.0]-------\n"),
+	printest(".1");
+	printest("001.");
+	printest("01.00");
+	printest("001.0");
+	printest("000000.0");
+	printest("1.1");
+	printest("0.00");
+	printest("0.0000000000");
+	printest("1.0000000000");
+	printest("-1");
+	printest("-0");
+	printest("-5");
+	printest("+0.01");
+	printest("-0.01");
+
+	return (0);
+}
