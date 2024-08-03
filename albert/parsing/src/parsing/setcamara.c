f90b751cc109fe98434126ff4ebacff2124d2c08
Alberto Pardo Marti <apardo-m@cbr1s3.42barcelona.com>
2024-08-03 16:04:42 +0200

    [miniRT] Parsing. 'MakefileParsing4'works with scenes 'subject1_ACL.rt' and 'subject1_ACL_ligth_bonus.rt'

--- albert/parsing/src/parsing/setcamara.c
+++ albert/parsing/src/parsing/setcamara.c
@@ -6,7 +6,7 @@
 /*   By: apardo-m <apardo-m@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/03 13:21:20 by apardo-m          #+#    #+#             */
-/*   Updated: 2024/08/03 13:58:47 by apardo-m         ###   ########.fr       */
+/*   Updated: 2024/08/03 15:21:49 by apardo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,10 @@
 
 void	setcamara(char **aelement, t_sceneinf *scene)
 {
-	printf("TODO: %s\n", aelement[0]);
-	printf("TODO: %p\n", scene->elemlst);
+	printf("set %s\n", aelement[0]);
+	scene->cam.isset = 1;
+	scene->cam.id = ft_strdup(aelement[0]);
+	setvectorinelemtscene(aelement[1], &scene->cam.viewpoint);
+	setvectorinelemtscene(aelement[2], &scene->cam.orient);
+	scene->cam.fov = ft_atof(aelement[3]);
 }
