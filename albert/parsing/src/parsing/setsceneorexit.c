fbe4b330adc319bd74cfd8f3817fbf995960222b
Alberto Pardo Marti <apardo-m@car1s6.42barcelona.com>
2024-08-06 17:53:45 +0200

    TODO : scene Jaume. Manage error in 'rt' files

--- albert/parsing/src/parsing/setsceneorexit.c
+++ albert/parsing/src/parsing/setsceneorexit.c
@@ -1,7 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   setsceneorexit.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apardo-m <apardo-m@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/06 16:58:55 by apardo-m          #+#    #+#             */
+/*   Updated: 2024/08/06 17:51:25 by apardo-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minirt.h"
 #include "testaux.h"
 
-static void exitifemptyfileoronlyspaces(int i, int j)
+static void	exitifemptyfileoronlyspaces(int i, int j)
 {
 	if (i == 0)
 		exiterror(EMPTY_FILE);
@@ -15,46 +27,71 @@ static void exitifemptyfileoronlyspaces(int i, int j)
  * - j : counter for no lines equal to "\0" or "\n"
  */
 
-static void setscenefromfd(int fd, t_sceneinf *scene)
+static void	setscenefromfd(int fd, t_sceneinf *scene)
 {
-	char		*line;
-	char		*cleanstr;
-	char 		**splitline;
-	int			i;
-	int			j;
+	char	*line;
+	char	*cleanstr;
+	char	**splitline;
+	int		i;
+	int		j;
 
 	i = 0;
 	j = 0;
 	line = get_next_line(fd);
-  	while (line)
+	while (line)
 	{
 		i++;
 		cleanstr = cleanstringspaces(line);
-		if (cleanstr[0] != '\0' && cleanstr[0] != '\n')
-			j++;
-		ft_printf("--------------------------------------------\n");
-		ft_printf("Linea Original  >%s", line);
-		ft_printf("Del extra spaces>%s", cleanstr);
-		splitline = ft_split(cleanstr, ' ');
-		putarraystr(splitline);
-		if (iselement(splitline))
+		if (cleanstr)
 		{
-			printf("\nElement is OK!\n");
-			setelementinscene(splitline, scene);
+			if (cleanstr[0] != '\0' && cleanstr[0] != '\n')
+				j++;
+			ft_printf("--------------------------------------------\n");
+			ft_printf("Linea Original  >%s", line);
+			ft_printf("Del extra spaces>%s", cleanstr);
+			splitline = ft_split(cleanstr, ' ');
+			if (splitline)
+			{
+				putarraystr(splitline);
+				if (iselement(splitline))
+				{
+					printf("\nElement is OK!\n");
+					setelementinscene(splitline, scene);
+				}
+				else
+				{
+					freearrstr(splitline);
+					free(cleanstr);
+					free(line);
+					exiterror("\nKO! (TODO free scene");
+				}
+				freearrstr(splitline);
+				free(cleanstr);
+				free(line);
+				line = get_next_line(fd);
+			}
+			else
+			{
+				free(cleanstr);
+				free(line);
+				printf("TODO : free scene en exit with error");
+			}
 		}
 		else
-			printf("\nKO!");
-		freearrstr(splitline);
-		free(cleanstr);
-		free(line);
-		line = get_next_line(fd);
+		{
+			free(line);
+			printf("TODO : free scene en exit with error");
+		}	
 	}
 	exitifcheckfails(close(fd), NO_CLOSE);
 	exitifemptyfileoronlyspaces(i, j);
 }
 
 /*
+ * void	setsceneorexit(int argc, char *scfile, t_sceneinf *scene)
+ * If file is right sceneinfo is created
  *
+ * else put error and exit
  */
 
 void	setsceneorexit(int argc, char *scfile, t_sceneinf *scene)
@@ -68,7 +105,6 @@ void	setsceneorexit(int argc, char *scfile, t_sceneinf *scene)
 		exitifcheckfails(fd, NO_OPEN);
 		printf("==== Initiaize empty scene ====");
 		ft_memset(scene, 0, sizeof(t_sceneinf));
-		//putsceneinfo(scene);
 		putsceneinfoinline(scene);
 		setscenefromfd(fd, scene);
 	}
