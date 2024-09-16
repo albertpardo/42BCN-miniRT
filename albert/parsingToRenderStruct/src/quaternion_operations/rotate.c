82934e7796e2465dae6919b54d39f792bea33d91
apardo-m <apardo-m@car15s3.42barcelona.com>
2024-09-16 15:11:00 +0200

    clean code and delete no necesary files

--- albert/parsingToRenderStruct/src/quaternion_operations/rotate.c
+++ albert/parsingToRenderStruct/src/quaternion_operations/rotate.c
@@ -6,45 +6,12 @@
 /*   By: jaucarri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 12:00:37 by jaucarri          #+#    #+#             */
-/*   Updated: 2024/09/16 14:55:12 by apardo-m         ###   ########.fr       */
+/*   Updated: 2024/09/16 15:08:35 by apardo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
-/*
-t_quaternion	quaternion_multiply(t_quaternion *q1, t_quaternion *q2)
-{
-	t_quaternion	result;
 
-	result.w = q1->w * q2->w - q1->x * q2->x - q1->y * q2->y - q1->z * q2->z;
-	result.x = q1->x * q2->w + q1->w * q2->x + q1->y * q2->z - q1->z * q2->y;
-	result.y = q1->w * q2->y - q1->x * q2->z + q1->y * q2->w + q1->z * q2->x;
-	result.z = q1->w * q2->z + q1->x * q2->y - q1->y * q2->x + q1->z * q2->w;
-	return (result);
-}
-
-static t_vector3	operation( t_vector3 vone, t_vector3 vtwo)
-{
-	t_vector3	ans;
-
-	ans.x = vone.y * vtwo.z - vone.z * vtwo.y;
-	ans.y = vone.z * vtwo.x - vone.x * vtwo.z;
-	ans.z = vone.x * vtwo.y - vone.y * vtwo.x;
-	return (ans);
-}
-
-static void	normal_by_magnitude(t_vector3 *vect, float mag)
-{
-	vect->x /= mag;
-	vect->y /= mag;
-	vect->z /= mag;
-}
-
-static float	vectmodule(t_vector3 vect)
-{
-	return (sqrtf(vect.x * vect.x + vect.y * vect.y + vect.z * vect.z));
-}
-*/
 /*
  * get_q1(t_vector3 desired)
  *
